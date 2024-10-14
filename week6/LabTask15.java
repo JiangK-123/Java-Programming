e1f02ef876e1b2566c669c8013508fcde33ecfd0
JiangK-123 <2984259709@qq.com>
2024-10-14 11:26:47 +0800

    Update LabTask15.java

--- week6/LabTask15.java
+++ week6/LabTask15.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class LabTask15 {
+    public static void main(String[] args) throws Exception {
+    System.out.println("Please iuput strings； ");
+    Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		boolean flag=true;
+    
+		for(int i=0;i<s.length()/2;i++){
+			if((s.charAt(i))!=(s.charAt(s.length()-1-i))){
+				flag=false;
+			}
+		}
+		if(flag==true){
+			System.out.println(s+" is parlindrome");
+		}else{
+			System.out.println(s+" is not parlindrome");
+		}
+		
+	}
+
+}
