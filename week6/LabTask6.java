cd920bc79ea578429eeab4386ce532bdb053bbb8
JiangK-123 <2984259709@qq.com>
2024-10-14 11:23:27 +0800

    Update LabTask6.java

--- week6/LabTask6.java
+++ week6/LabTask6.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+
+public class LabTask6 {
+    public static void main(String[] args) throws Exception {
+      
+int data;
+int sum=0;
+
+// create a scanner
+Scanner input = new Scanner(System.in);
+
+// Keep reading data until the input is o
+
+do{
+
+// Read the next data
+
+System.out.print("Enter an integer(the input ends if it is o):");
+
+data = input.nextInt();
+
+sum+= data;
+
+}while(data!=0);
+
+System.out.println("The sum is "+ sum);
+
+      
+    }
+
+}
+    
 
