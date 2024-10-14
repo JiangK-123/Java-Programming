6bb7858b09fcf08de12816f1fc916981303c8fde
JiangK-123 <2984259709@qq.com>
2024-10-14 08:32:58 +0800

    Update Task19.java

--- week5/Task19.java
+++ week5/Task19.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class Task19{  
+    
+public static void main(String[] args){
+    int count=5;
+    double amount=45.56;
+    System.out.printf("count is %d and amount is %f",count,amount);
+    System.out.println("\n");
+    System.out.printf("%,8d %,10.1f\n",12345678,1234567.263);
+}
+}
+
+
 
