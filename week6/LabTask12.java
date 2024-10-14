7dae935c8718f32b7cca9e8e8a3d538e0ecb4f56
JiangK-123 <2984259709@qq.com>
2024-10-14 11:25:42 +0800

    Update LabTask12.java

--- week6/LabTask12.java
+++ week6/LabTask12.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
+
+public class LabTask12 {
+    public static void main(String[] args) throws Exception {
+
+     int count=0;  
+     int number=0;       
+     for(int i = 2;count < 50; i++)
+    	 
+	     {
+	    	 int isprime = 1;         
+	    	 for(int n=2;n<i;n++)   
+	    	 { 
+	    		 if(i%n==0)           
+	    		 {
+	    			 isprime=0;
+	    			 break;
+	    		 }
+	    	 }
+	    		 if(isprime==1) 
+	    		 {
+	    			 System.out.print(i+" ");
+	    			 number=number+1; 
+	    			 count=count+1;
+	    			 	 if(number%10==0)
+	    			 	 {
+	    				 System.out.println();
+	    		         }
+	    		 }
+	     }
+		
+	 }
+
+        }
+
+
+    
 
