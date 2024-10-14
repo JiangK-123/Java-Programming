48c69c32c078b37df21ae36ee05f681a9c9299d4
JiangK-123 <2984259709@qq.com>
2024-10-14 11:26:22 +0800

    Update LabTask14.java

--- week6/LabTask14.java
+++ week6/LabTask14.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class LabTask14 {
+    public static void main(String[] args) throws Exception {
+
+  double m=10000;
+  int count= 0;
+  while(m<20000)
+  {
+   m*=(1+0.07);
+   count++;
+  }
+   System.out.println("The Tuition will be double in "+count);
+   System.out.println("The Tuition will be "+m+" in "+count+" year ");
+ }
+
+
+		}
