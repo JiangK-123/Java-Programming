eda6000074cf7f29afed01faf4238efd5d9cddc7
JiangK-123 <2984259709@qq.com>
2024-10-14 11:23:10 +0800

    Update LabTask5.java

--- week6/LabTask5.java
+++ week6/LabTask5.java
@@ -1,1 +1,27 @@
 
+import java.util.Scanner;
+
+public class LabTask5 {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner input = new Scanner(System.in);
+        
+        // Read an initial data
+        
+        System.out.print("Enter an integer(the input ends if it is 0):");
+        
+        int data = input.nextInt();
+        int sum=0;
+
+        while(data!=0){
+
+        sum += data;
+        System.out.print("Enter an integer(the input ends if it is 0):");
+
+        data = input.nextInt();}
+        System.out.println("The sum is "+ sum);
+         
+    }
+
+}
+    
