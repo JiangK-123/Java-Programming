63f06227fa296da7721332e955b82f9cbe1ef511
JiangK-123 <2984259709@qq.com>
2024-10-14 11:24:43 +0800

    Update LabTask9.java

--- week6/LabTask9.java
+++ week6/LabTask9.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+public class LabTask9 {
+    public static void main(String[] args) throws Exception {
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("\n");
+        }
+        }
+
+}
+    
 
