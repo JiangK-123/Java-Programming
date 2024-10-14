e3ba7986abeee63503af53756a2a0dae6f439e1f
JiangK-123 <2984259709@qq.com>
2024-10-14 08:30:12 +0800

    Update Task9.java

--- week5/Task9.java
+++ week5/Task9.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+public class Task9 {  
+    
+public static void main(String[] args){
+
+    
+
+String message ="welcome to Java";
+
+System.out.println("The first character in message is "+ message.charAt(0));
+
+
+}
+}
+
 
