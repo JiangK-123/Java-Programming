79c209e058c81a45f8d985f68e1be75f4ffe9bd2
JiangK-123 <2984259709@qq.com>
2024-10-14 08:32:11 +0800

    Update Task16.java

--- week5/Task16.java
+++ week5/Task16.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
+public class Task16 {  
+    
+public static void main(String[] args){
+
+System.out.println("welcome to Java".indexOf("w"));
+
+System.out.println("Welcome to Java".indexOf("o"));
+
+System.out.println("Welcome to Java".indexOf("o",5));
+
+System.out.println("welcome to Java".indexOf("come"));
+System.out.println("welcome to Java".indexOf("Java",5));
+
+System.out.println("Welcome to Java".indexOf("java",5));
+
+//lastIndex0f
+
+System.out.println("welcome to Java".lastIndexOf("w"));
+
+System.out.println("Welcome to Java".lastIndexOf("o"));
+
+System.out.println("Welcome to Java".lastIndexOf("o",5));
+
+System.out.println("welcome to Java".lastIndexOf("come"));
+
+System.out.println("welcome to Java".lastIndexOf("Java",5));
+
+System.out.println("welcome to Java".lastIndexOf("java",5));
+
+
+}
+}
+
 
