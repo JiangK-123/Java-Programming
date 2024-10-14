55185cd829af440ef6cf78b0390f1eda64fd29b7
JiangK-123 <2984259709@qq.com>
2024-10-14 08:34:21 +0800

    Update Task23.java

--- week5/Task23.java
+++ week5/Task23.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Task23{
+public static void main(String[] args){
+		double area;
+		double s;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the length from the center to a vertex:");
+		double r = input.nextDouble();
+		s=2*r*Math.sin(Math.PI/5);
+		area=(5*Math.pow(s,2))/(4*Math.tan(Math.PI/5));
+		System.out.printf("The area of the pentagon is %.2f",area);
+		input.close();
+	}
+}
+
+
 
