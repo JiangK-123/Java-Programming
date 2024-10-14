7505376344bf25bea0f9581dbc8a495924b903a0
JiangK-123 <2984259709@qq.com>
2024-10-14 08:33:19 +0800

    Update Task20.java

--- week5/Task20.java
+++ week5/Task20.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
+public class Task20{  
+    
+public static void main(String[] args){
+    System.out.printf("%8d%8s%8.1f\n",1234,"Java",5.63);
+    System.out.printf("%-8d%-8s%-8.1f\n",1234,"Java",5.63);
+
+}
+}
+
+
 
