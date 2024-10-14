e8abb395356e3c38f9e352fc9303e8cd499fe703
JiangK-123 <2984259709@qq.com>
2024-10-14 11:24:20 +0800

    Update LabTask8.java

--- week6/LabTask8.java
+++ week6/LabTask8.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
+
+public class LabTask8 {
+    public static void main(String[] args) throws Exception {
+        
+        
+        System.out.println("Multiplication Table");
+        System.out.print("");
+        
+        for(int j=1;j<=9;j++) System.out.print("  "+j);       
+        System.out.println("\n--------------------------------");
+         
+        
+        for(int i=1;i<=9;i++){
+        
+        System.out.print(i+" | ");
+        
+        for(int j=1;j<=9;j++){
+        
+        // Display the product and align properly
+        
+        System.out.printf("%5d",i*j);
+        }
+        System.out.println();
+          
+    }
+        }
+
+}
+    
 
