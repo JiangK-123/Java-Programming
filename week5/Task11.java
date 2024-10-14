09ed7d23842c8d51bb80ede20e966fb2424144b2
JiangK-123 <2984259709@qq.com>
2024-10-14 08:30:50 +0800

    Update Task11.java

--- week5/Task11.java
+++ week5/Task11.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+public class Task11 {  
+    
+public static void main(String[] args){
+   
+// Three strings are concatenated
+
+String message="welcome"+"to"+"Java";
+System.out.println(message);
+// string chapter is concatenated with number 2
+
+String s="Chapter"+2;// s becomes chapter2
+System.out.println(s);
+// string supplement is concatenated with character B
+String s1="Supplement"+'B';// s1 becomes supplementB
+System.out.println(s1);
+
+}
+}
+
 
