7e88228d0ad7dcc76d189b6908571646625153a4
JiangK-123 <2984259709@qq.com>
2024-10-14 08:29:58 +0800

    Update Task8.java

--- week5/Task8.java
+++ week5/Task8.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
+public class Task8 {  
+    
+public static void main(String[] args){
+
+    String message ="we1come to Java";
+    System.out.println("The 1ength of"+ message +" is "+ message.length());
+    
+}
+}
+
 
