31cf350b016527fe2378d94274f59e98be38d8c2
JiangK-123 <2984259709@qq.com>
2024-10-14 08:29:05 +0800

    Update Task5.java

--- week5/Task5.java
+++ week5/Task5.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+public class Task5 {  
+    
+public static void main(string[] args){
+
+char ch='B';
+
+if(ch>='A'&&ch<='Z'){
+
+System.out.println(ch +" is an uppercase letter");
+}
+
+else if(ch>='a'&&ch<='z'){
+
+system.out.println(ch +" is a lowercase letter");
+}
+
+else if(ch>='0'&&ch<='9'){
+
+System.out.println(ch +"is a numeric character");
+}
+
+    
+}
+}
 
