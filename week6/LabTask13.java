8ed4e5a0ee6416e95735b272d78db7eb99fd8dd4
JiangK-123 <2984259709@qq.com>
2024-10-14 11:25:59 +0800

    Update LabTask13.java

--- week6/LabTask13.java
+++ week6/LabTask13.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+
+public class LabTask13 {
+    public static void main(String[] args) throws Exception {
+		
+			Scanner scan = new Scanner(System.in);
+			
+			System.out.println("Enter first integer: ");
+			int a = scan.nextInt();
+			
+			System.out.println("Enter second integer: ");
+			int b = scan.nextInt();
+
+			while ((a % b) != 0) {
+				int c = a % b;
+				a = b;
+				b = c;
+			}
+			System.out.println("The greatest common divisor for " + a+" and "+b+" is "+b);
+		}
+		
+	 }
+
+	
+
+
+    
 
