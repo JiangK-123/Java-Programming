549319a1ea1924476d4b0692299abf69c48a4d63
JiangK-123 <2984259709@qq.com>
2024-10-14 08:29:40 +0800

    Update Task7.java

--- week5/Task7.java
+++ week5/Task7.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+public class Task7 {  
+    
+public static void main(String[] args){
+
+System.out.println("isDigit('a')is"+ Character.isDigit('a'));
+
+System.out.println("isLetter('a')is"+ Character.isLetter('a'));
+
+System.out.println("isLowerCase('a')is"+ Character.isLowerCase('a'));
+
+System.out.println("isUpperCase('a')is "+ Character.isUpperCase('a'));
+
+System.out.println("toLowercase('T')is "+ Character.toLowerCase('T'));
+
+System.out.println("toupperCase('q')is "+ Character.toUpperCase('q'));
+}
+}
+
 
