c26f0e25770c36c684d112fe0181fff77118b7ed
JiangK-123 <2984259709@qq.com>
2024-10-14 11:25:05 +0800

    Update LabTask10.java

--- week6/LabTask10.java
+++ week6/LabTask10.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+
+public class LabTask10 {
+    public static void main(String[] args) throws Exception {
+
+            int sum=0;
+            
+            int number = 0;
+
+            while(number< 20){
+            number++;
+            sum += number;
+            if(sum>= 100)
+            break;
+            System.out.println("The number is"+ number);
+            System.out.println("The sum is"+ sum);
+            
+    }
+
+}
+}
+    
 
