561a3f2f56a194b65180321521ed882bb33eaf96
JiangK-123 <2984259709@qq.com>
2024-10-14 08:31:38 +0800

    Update Task14.java

--- week5/Task14.java
+++ week5/Task14.java
@@ -1,1 +1,43 @@
+import java.util.Scanner;
+public class Task14 {  
+    
+public static void main(String[] args){
+ 
+String s1="welcome to Java";
+
+String s2="welcome toJava";
+
+String s3 ="welcome toC++";
+
+// equals
+
+System.out.println(s1.equals(s2));
+System.out.println(s1.equals(s3));
+
+// compareTo
+
+System.out.println(s1.compareTo(s2));
+
+System.out.println(s1.compareTo(s3));
+
+// startswith
+
+System.out.println(s1.startsWith("we"));
+
+System.out.println(s1.startsWith("we"));
+
+// endswith
+
+System.out.println(s1.endsWith("va"));
+
+System.out.println(s1.endsWith("v"));
+
+// contain
+
+System.out.println(s1.contains("to"));
+
+System.out.println(s1.contains("To"));
+}
+}
+
 
