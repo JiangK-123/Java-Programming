21645c7e703f29a4ced37b9072508d4ddc477afe
JiangK-123 <2984259709@qq.com>
2024-10-14 08:31:55 +0800

    Update Task15.java

--- week5/Task15.java
+++ week5/Task15.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+public class Task15 {  
+    
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter two cities: ");
+String city1=sc.next();
+String city2=sc.next();
+int a=(city1.compareTo(city2));
+if(a<0){
+    System.out.println(city1+city2);
+}
+    else System.out.println(city2+city1);
+}
+}
+
 
