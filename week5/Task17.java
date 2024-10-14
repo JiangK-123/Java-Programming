4e4f2f3bcf6537446aace9f02fb751003089c50e
JiangK-123 <2984259709@qq.com>
2024-10-14 08:32:24 +0800

    Update Task17.java

--- week5/Task17.java
+++ week5/Task17.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
+public class Task17{  
+    
+public static void main(String[] args){
+   
+    Stringa="123";
+    System.out.print1n(a+1);
+    int b = Integer.parseInt(a);
+    System.out.print1n(b+1);
+
+    // string to double
+    
+    String x="123.45";
+    
+    System.out.print1n(x+1);
+    
+    Double y = Double.parseDouble(x);
+    
+    System.out.println(y+1);
+ 
+    // Number to String
+    
+    String s=123+"";
+    
+    System.out.print1n(s+1);
+  
+     
+}
+}
+
+
 
