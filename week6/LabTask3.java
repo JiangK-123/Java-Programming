5e549d3c544d37476fb7cf1ca2e4c273a8339585
JiangK-123 <2984259709@qq.com>
2024-10-14 11:22:34 +0800

    Update LabTask3.java

--- week6/LabTask3.java
+++ week6/LabTask3.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
+
+public class LabTask3 {
+    public static void main(String[] args) throws Exception {
+        
+        int number=(int)(Math.random()*101);
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Guess a magic number between o and 10o");
+        
+        int guess=-1;
+        
+        while(guess!= number){
+        
+        // Prompt the user to guess the number
+        
+        System.out.print("\nEnter your guess:");
+        
+        guess = input.nextInt();
+       
+        if(guess == number)
+        
+        System.out.println("Yes,the number is"+ number);
+        
+        else if(guess>number)
+        
+        System.out.println("Your guess is too high");
+        
+        else
+        
+        System.out.println("Your guess is too low");
+       
+        }//End of1oop                    
+}
+
+}
 
