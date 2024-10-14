5e803d0878d7ab0fdd70d82a8a14b6bf4f8383af
JiangK-123 <2984259709@qq.com>
2024-10-14 08:28:09 +0800

    Update Task2.java

--- week5/Task2.java
+++ week5/Task2.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
+public class Task2 {  
+    public static void main(String[] args) {
 
+
+        
+        System.out.println("The value of exp(3.5):"+
+        
+        Math.exp(3.5));
+        
+        System.out.println("The value of log(3.5):"+
+        
+        Math.log(3.5));
+        
+        System.out.println("The value of log1o(3.5):"+
+        Math.log10(3.5));
+        
+        System.out.println("The value of power(3,2):"+
+        
+        Math.pow(3,2));
+        
+        System.out.println("The value of sqrt(4):"+
+        
+        Math.sqrt(4));
+        
+        System.out.println("The value of sqrt(1o.5):"+
+        
+        Math.sqrt(10.5));
+        
+        
+        
+
+
+    }
+}
