cc23b7e78ae74c2fbe09bea288f64bc7d6b0969f
JiangK-123 <2984259709@qq.com>
2024-10-14 11:24:05 +0800

    Update LabTask7.java

--- week6/LabTask7.java
+++ week6/LabTask7.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+
+public class LabTask7 {
+    public static void main(String[] args) throws Exception {
+            int sum=0;
+            int j=10;
+            for(int i=0;i<10;i++)
+            {
+                sum += i;           
+                if(i<j) System.out.println(i);           
+            else  System.out.println(j);
+
+            }            
+            while(j<10);
+{
+         j++;
+}
+            do{        
+            j++;           
+            }while(j<10);           
+    }
+
+}
+    
 
