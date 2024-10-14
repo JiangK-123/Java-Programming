1fa76db5ef1098e997905552b3d3dac1e89e4513
JiangK-123 <2984259709@qq.com>
2024-10-14 08:30:32 +0800

    Update Task10.java

--- week5/Task10.java
+++ week5/Task10.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+public class Task10 {  
+    
+public static void main(String[] args){
+
+    
+    
+    
+    String message ="welcome to Java";
+    
+    System.out.println("The message in lowecase is:" + message.toLowerCase());
+    
+    System.out.println("The message in UppERcAsE is: " + message.toUpperCase());
+    
+
+}
+}
+
 
