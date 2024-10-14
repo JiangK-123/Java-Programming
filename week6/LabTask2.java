e71b05025476bf5ed353600d81494ee580b0753a
JiangK-123 <2984259709@qq.com>
2024-10-14 11:22:15 +0800

    Update LabTask2.java

--- week6/LabTask2.java
+++ week6/LabTask2.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
+
+public class LabTask2 {
+    public static void main(String[] args) throws Exception {
+       
+        int number1=(int)(Math.random()*10);
+        
+        int number2=(int)(Math.random()*10);
+        
+        
+        
+        Scanner input = new Scanner(System.in);
+       
+        System.out.print("whatis"+number1+"+"+number2+"?");
+        
+        int answer = input.nextInt();
+        
+        
+        while(number1+ number2!=answer){
+        
+        System.out.print("wrong answer.Try again. what is "+number1+"+"+number2+"?");
+        
+        answer = input.nextInt();
+        
+        System.out.println("You got it!");
+       
+                   
+}
+}
+}
 
