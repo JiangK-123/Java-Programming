59c2b695a648e386544355f8473acf9c614b3a9b
JiangK-123 <2984259709@qq.com>
2024-10-21 10:44:15 +0800

    Update LabTask05.java

--- week7/LabTask05.java
+++ week7/LabTask05.java
@@ -1,1 +1,12 @@
-
+import java.util.Scanner;
+public class LabTask05{
+    public static void main(String[] args) {
+            nPrintln("Welcome to Java",5);
+            nPrintln("Computer Science",15);
+            }
+        public static void nPrintln(String message,int n) {
+            for(int i=0;i<n;i++){
+                System.out.println(message);
+            }
+        }
+        }
