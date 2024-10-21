7a199c78d0076c246a7485ddec5abd77081e996d
JiangK-123 <2984259709@qq.com>
2024-10-21 12:57:31 +0800

    Update LabTask10.java

--- week7/LabTask10.java
+++ week7/LabTask10.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+public class LabTask10{
+    public static void main(String[] args) {
+        final int NUMBER_OF_CHARS=175;
+        final int CHAR_PER_LTNE=25;
 
+        for(int i=0;i<NUMBER_OF_CHARS;i++){
+            char ch=RandomCharacter.getRandomLowCaseLetter();
+            if((i+1)%(CHAR_PER_LTNE)==0)
+            System.out.println(ch);
+            else
+            System.out.print(ch);
+        }
+        
+    }
+    public class RandomCharacter{
+        public static char getRandomCharacter(char ch1,char ch2){
+            return(char)(ch1+Math.random()*(ch2-ch1+1));
+        }
+        public static char getRandomLowCaseLetter(){
+            return getRandomCharacter('a','z');
+        }
+        public static char getRandomUpperCaseLetter(){
+            return getRandomCharacter('A','Z');
+        }
+    }
+
+}
