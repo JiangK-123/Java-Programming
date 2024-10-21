c2516be8b99031559a268998afe71ea510d5c2ca
JiangK-123 <2984259709@qq.com>
2024-10-21 10:46:25 +0800

    Update LabTask06.java

--- week7/LabTask06.java
+++ week7/LabTask06.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+public class LabTask06{
+    public static void main(String[] args) {
 
+        int x=1;
+        
+        System.out.println("Before the cal1,x is "+ x);
+        
+        increment(x);
+        
+        System.out.println("After the cal1,x is "+x);}
+        
+        
+        public static void increment(int n){
+        
+        n++;
+        
+        System.out.println("n inside the method is "+ n);
+        
+        
+        
+        }
+    }
