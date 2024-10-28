7abc31d8faca59d672cb6d6d95e91e21105df137
JiangK-123 <2984259709@qq.com>
2024-10-28 09:46:27 +0800

    Update Task02.java

--- week8/Task02.java
+++ week8/Task02.java
@@ -1,1 +1,17 @@
+package com.HJK.hello;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task02{
+    public static void main(String[] args) {
+        double[] mylist=new double[5];
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter "+mylist.length+" Values");
+        for (int i = 0; i < mylist.length; i++) {
+            mylist[i]=input.nextDouble();
+        }
+        input.close();
+    }
+}
 
