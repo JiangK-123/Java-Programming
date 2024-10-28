53731b094c42c6ea20c0c26661a9825e854d0d54
JiangK-123 <2984259709@qq.com>
2024-10-28 10:54:18 +0800

    Update Task18.java

--- week8/Task18.java
+++ week8/Task18.java
@@ -1,1 +1,28 @@
+package com.HJK.hello;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task18 {
+    public static void main(String[] args) {
+        int[] list1={1,2,3,4,5};
+        int[] list2=reverse(list1);
+        System.out.println("List1: ");
+        for(int value1: list1){
+            System.out.println(value1+" ");
+        }
+        System.out.println("\nList2: ");
+        for(int value2: list2){
+            System.out.println(value2+" ");
+        }
+    }
+
+    public static int[] reverse(int[] list){
+        int[] result=new int[list.length];
+        for(int i=0,j=result.length-1;i<list.length;i++,j--){
+            result[j]=list[i];
+        }
+        return result;
+    }
+}
 
