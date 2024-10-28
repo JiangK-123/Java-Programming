680f563bcf382b59fba06f00343f9591efd4a84a
JiangK-123 <2984259709@qq.com>
2024-10-28 11:07:08 +0800

    Update Task20.java

--- week8/Task20.java
+++ week8/Task20.java
@@ -1,1 +1,20 @@
+package com.HJK.hello;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task20 {
+    public static void main(String[] args) {
+        int[] list = {1, 4, 8, 5, 9, 7};
+        int i = linearsearch(list,8);
+        System.out.println("The key is found at index: " + i);
+    }
+        public static int linearsearch(int[] list,int key){
+            for(int i=0;i<list.length;i++){
+                if(key==list[i]){
+                    return i;
+                }
+        }
+            return -1;
+    }
+}
