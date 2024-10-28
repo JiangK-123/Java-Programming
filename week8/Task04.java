a36f29fd79fa89c910f7b37abbb8eb1bba7b024a
JiangK-123 <2984259709@qq.com>
2024-10-28 09:51:01 +0800

    Update Task04.java

--- week8/Task04.java
+++ week8/Task04.java
@@ -1,1 +1,18 @@
+package com.HJK.hello;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task04{
+    public static void main(String[] args) {
+        double[] mylist=new double[5];
+        for (int i = 0; i < mylist.length; i++) {
+            mylist[i]=Math.random()*10;
+        }
+
+        for (int i = 0; i < mylist.length; i++) {
+            System.out.println(mylist[i]+" ");
+        }
+    }
+}
 
