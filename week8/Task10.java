3456490b1f788c1739c6774722b8ca8e2f1a8fb4
JiangK-123 <2984259709@qq.com>
2024-10-28 10:16:30 +0800

    Update Task10.java

--- week8/Task10.java
+++ week8/Task10.java
@@ -1,1 +1,18 @@
+package com.HJK.hello;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String[] args) {
+        String[] months={"January","February","March","April","May","June",
+        "July","August","September","October","November","December"};
+
+        System.out.println("Enter a month number (1 to 12): ");
+        Scanner input=new Scanner(System.in);
+        int monthNumber=input.nextInt();
+        System.out.println("The month is "+months[monthNumber-1]);
+        input.close();
+    }
+}
 
