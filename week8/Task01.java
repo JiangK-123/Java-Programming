43dee60da01244e4ca99060f8a2c4eabe021ddae
JiangK-123 <2984259709@qq.com>
2024-10-28 09:42:36 +0800

    Update Task01.java

--- week8/Task01.java
+++ week8/Task01.java
@@ -1,1 +1,15 @@
+package com.HJK.hello;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        int[] values = new int[5];
+        for (int i = 1; i < 5; i++) {
+            values[i] = i+values[i-1];
+        }
+        values[0]=values[1]+values[4];
+    }
+}
 
