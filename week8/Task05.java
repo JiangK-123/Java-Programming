11cad5138dde067d494812b247db77e1e0a3337b
JiangK-123 <2984259709@qq.com>
2024-10-28 09:53:47 +0800

    Update Task05.java

--- week8/Task05.java
+++ week8/Task05.java
@@ -1,1 +1,22 @@
+package com.HJK.hello;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task05 {
+    public static void main(String[] args) {
+        double[] mylist=new double[5];
+        for (int i = 0; i < mylist.length; i++) {
+            mylist[i]=Math.random()*10;
+        }
+
+        double total=0;
+        for (int i = 1; i < mylist.length; i++) {
+            System.out.println(mylist[i]+" ");
+            total+=mylist[i];
+        }
+        System.out.println();
+        System.out.println("Sum of all elements is: "+total);
+    }
+}
 
