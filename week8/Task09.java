1628a923053f0ca52857d013b61ad8c5d8923ce6
JiangK-123 <2984259709@qq.com>
2024-10-28 10:12:14 +0800

    Update Task09.java

--- week8/Task09.java
+++ week8/Task09.java
@@ -1,1 +1,34 @@
+package com.HJK.hello;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task09 {
+    public static void main(String[] args) {
+        int[] mylist = new int[5];
+
+        System.out.println("Array before shuffling: ");
+        for (int i = 0; i < mylist.length; i++) {
+            mylist[i] = (int) (Math.random() * mylist.length);
+        }
+
+        System.out.println("Array after shifting: ");
+
+
+        for (int i = 0; i < mylist.length; i++) {
+            System.out.println(mylist[i] + " ");
+        }
+        int temp = mylist[0];
+        for (int i = 1; i < mylist.length; i++) {
+            mylist[i - 1] = mylist[i];
+        }
+        mylist[mylist.length - 1] = temp;
+
+        System.out.println("\nArray after shifting: ");
+        for (int i = 0; i < mylist.length; i++) {
+            System.out.println(mylist[i] + " ");
+        }
+
+    }
+}
 
