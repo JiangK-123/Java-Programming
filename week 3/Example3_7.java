fd629a51d8df1ef427a31919e6088af1fd1a19ff
JiangK-123 <2984259709@qq.com>
2024-11-12 00:52:00 +0800

    Update Example3_7.java

--- week 3/Example3_7.java
+++ week 3/Example3_7.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Example3_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your score: ");
+        double score = sc.nextDouble();
+
+        if (score >= 90) {
+            System.out.println("Grade is A");
+        }
+        else if (score >= 80) {
+            System.out.println("Grade is B");
+        }
+        else if (score >= 70) {
+            System.out.println("Grade is C");
+        }
+        else if (score >= 60) {
+            System.out.println("Grade is D");
+        }
+        else
+            System.out.println("Grade is E");
+    }
+}
