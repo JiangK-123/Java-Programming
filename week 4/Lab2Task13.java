98bad6abbc84b13d0afe3d097453528b3f7d52c5
JiangK-123 <2984259709@qq.com>
2024-11-12 01:24:01 +0800

    Update Lab2Task13.java

--- week 4/Lab2Task13.java
+++ week 4/Lab2Task13.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+public class Lab2Task13 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the score : ");
+        double score = input.nextDouble();
+        if (score >= 90) {
+            score = score * 1.03;
+        } else score = score * 1.01;
+        System.out.println("your payment is " + score);
+    }
+
+}
