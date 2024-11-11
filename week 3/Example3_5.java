1255c408409398e95e462d246723c0407a8f9b0e
JiangK-123 <2984259709@qq.com>
2024-11-12 00:50:02 +0800

    Update Example3_5.java

--- week 3/Example3_5.java
+++ week 3/Example3_5.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Example3_5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the score : ");
+        double score = input.nextDouble();
+        if (score >= 90) {
+            score = score * 1.03;
+        }
+        else score = score * 1.01;
+        System.out.println("your payment is " + score);
+    }
+
+}
