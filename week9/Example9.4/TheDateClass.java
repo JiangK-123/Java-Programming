89e9ab9bba4df383238cc637d19051b72d56290d
JiangK-123 <2984259709@qq.com>
2024-11-11 18:50:49 +0800

    Update TheDateClass.java

--- week9/Example9.4/TheDateClass.java
+++ week9/Example9.4/TheDateClass.java
@@ -1,1 +1,11 @@
+package com.HJK.hello;
 
+import java.util.Date;
+
+public class TheDateClass {
+    public static void main(String[] args) {
+        Date date=new Date();
+        System.out.println("The elapsed time since Jan 1,1970 is "+date.getTime()+"milliseconds");
+        System.out.println(date.toString());
+    }
+}
