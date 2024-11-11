4e16978f9ff30ca7221ad6a43ee31882fcc24a12
JiangK-123 <2984259709@qq.com>
2024-11-12 01:19:52 +0800

    Update and rename Lab2Task8.java to Lab2Task08.java

--- week 4/Lab2Task08.java
+++ week 4/Lab2Task08.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lab2Task8 {
+public class Lab2Task08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the side:");
