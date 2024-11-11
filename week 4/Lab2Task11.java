6fbd509bf13f00d5bf04d1bf9473da5ea6d9d128
JiangK-123 <2984259709@qq.com>
2024-11-12 01:21:51 +0800

    Update Lab2Task11.java

--- week 4/Lab2Task11.java
+++ week 4/Lab2Task11.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
 
+public class Lab2Task11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter purchase amount: ");
+        double account = sc.nextDouble();
+        double tax = account * 0.06;
+        System.out.print("Sales tax is $");
+        System.out.printf("%.2f", tax);
+    }
+}
