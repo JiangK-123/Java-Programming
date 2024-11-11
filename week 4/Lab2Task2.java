bf5ff064dcea42b47136d03a4a279386144e52f9
JiangK-123 <2984259709@qq.com>
2024-11-12 01:12:31 +0800

    Update Lab2Task2.java

--- week 4/Lab2Task2.java
+++ week 4/Lab2Task2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LabTask2 {
+public class Lab2Task2 {
         public static void main(String[] args) {
             Scanner scanner=new
             Scanner(System.in);
