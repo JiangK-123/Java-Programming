3d13274919d62d09ec841d3724eec82107531079
JiangK-123 <2984259709@qq.com>
2024-11-12 01:18:11 +0800

    Update and rename Lab2Task1.java to Lab2Task01.java

--- week 4/Lab2Task01.java
+++ week 4/Lab2Task01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Lab2Task1 {
+public class Lab2Task01 {
         public static void main(String[] args) {
             double miles=100;
             double KILOMETERS_PER_MILE=1.609;
