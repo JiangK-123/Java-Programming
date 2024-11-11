d5b07c549a9d7fe167ff4d48fe339177625ba09b
JiangK-123 <2984259709@qq.com>
2024-11-12 01:40:21 +0800

    Update and rename LabTask1.java to LabTask01.java

--- week6/LabTask01.java
+++ week6/LabTask01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LabTask1 {
+public class LabTask01 {
     public static void main(String[] args) throws Exception {
         int sum=0,i=1;
         while(i<10){
