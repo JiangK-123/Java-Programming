b95e17a1e673d3a1d7c636cc771710766b40b173
JiangK-123 <2984259709@qq.com>
2024-11-12 01:19:13 +0800

    Update and rename Lab2Task5.java to Lab2Task05.java

--- week 4/Lab2Task05.java
+++ week 4/Lab2Task05.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Lab2Task5 {
+public class Lab2Task05 {
         public static void main(String[] args) {                    
            
                     Scanner sc = new Scanner(System.in);                   
