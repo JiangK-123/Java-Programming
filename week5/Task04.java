7584af99c3c294c8297d954d3ee48677c4752367
JiangK-123 <2984259709@qq.com>
2024-11-12 01:36:12 +0800

    Update and rename Task4.java to Task04.java

--- week5/Task04.java
+++ week5/Task04.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Task4 {  
+public class Task04 {  
     public static void main(String[] args) {      
         System.out.println(Math.max(2,3));
         System.out.println(Math.min(2.5,4.6));
