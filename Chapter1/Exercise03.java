28cbe1e0b4820def045d85c9f0889672ec081981
JiangK-123 <2984259709@qq.com>
2024-11-12 23:33:18 +0800

    Update Exercise03.java

--- Chapter1/Exercise03.java
+++ Chapter1/Exercise03.java
@@ -1,1 +1,10 @@
+package com.HJK.hello;
 
+public class Exercise03 {
+    public static void main(String[] args) {
+        System.out.println("   J     A     V     V     A");
+        System.out.println("   J    A A     V   V     A A");
+        System.out.println("J  J   AAAAA     V V     AAAAAA");
+        System.out.println(" JJ   A     A     V     A      A");
+    }
+}
