484d4a9ca905c97ef07c6b887fe0f5a326d3e4a5
JiangK-123 <2984259709@qq.com>
2024-11-13 00:06:51 +0800

    Update Exercise11.java

--- Chapter1/Exercise11.java
+++ Chapter1/Exercise11.java
@@ -1,1 +1,16 @@
+package com.HJK.hello;
 
+import java.util.Scanner;
+
+public class Exercise11 {
+    public static void main(String[] args) {
+
+        System.out.println(365*24*60*60/7.0+365*24*60*60/45.0-365*24*60*60/13.0);
+        System.out.println("First-year population："+(312_032_486+2_780_096));
+        System.out.println("Second year population："+(312_032_486+ 2 * 2_780_096));
+        System.out.println("Third year population："+(312_032_486+ 3 * 2_780_096));
+        System.out.println("Fourth year population："+(312_032_486+ 4 * 2_780_096));
+        System.out.println("Fifth year population："+(312_032_486+ 5 * 2_780_096));
+
+    }
+}
