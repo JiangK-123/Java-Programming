5505c2417110786733c0e4978afa5aa32dc4605a
JiangK-123 <2984259709@qq.com>
2024-11-12 23:47:30 +0800

    Update Exercise06.java

--- Chapter1/Exercise06.java
+++ Chapter1/Exercise06.java
@@ -1,1 +1,11 @@
+package com.HJK.hello;
 
+public class Exercise06 {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i = 1; i <= 9; i++) {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
