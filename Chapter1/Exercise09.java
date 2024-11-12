d54c8339c416578609671bd7ef18ad0ddca0b790
JiangK-123 <2984259709@qq.com>
2024-11-12 23:57:47 +0800

    Update Exercise09.java

--- Chapter1/Exercise09.java
+++ Chapter1/Exercise09.java
@@ -1,1 +1,12 @@
+package com.HJK.hello;
 
+import java.util.Scanner;
+
+public class Exercise09 {
+    public static void main(String[] args) {
+
+        System.out.println("Area：" + 4.5 * 7.9);
+        System.out.println("Circumference：" + 2 * (4.5 + 7.9));
+
+    }
+}
