f27f11ff5c9d28658f21a1a170ae4e2d2aaedb49
JiangK-123 <2984259709@qq.com>
2024-11-12 23:54:54 +0800

    Update Exercise08.java

--- Chapter1/Exercise08.java
+++ Chapter1/Exercise08.java
@@ -1,1 +1,21 @@
+package com.HJK.hello;
 
+import java.util.Scanner;
+
+public class Exercise08 {
+    public static void main(String[] args) {
+
+
+                System.out.println("Please enter the radius of the circle: ");
+                Scanner input = new Scanner(System.in);
+                double r = input.nextDouble();
+
+                double C = 2 * r * Math.PI;
+                double S = r * r * Math.PI;
+
+                System.out.println("The area is：" + S);
+                System.out.println("The circumference is：" + C);
+
+
+    }
+}
