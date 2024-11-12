4bd4c5f4b700aea41d6070b56291d89f33b19bff
JiangK-123 <2984259709@qq.com>
2024-11-13 00:02:17 +0800

    Update Exercise10.java

--- Chapter1/Exercise10.java
+++ Chapter1/Exercise10.java
@@ -1,1 +1,11 @@
+package com.HJK.hello;
 
+import java.util.Scanner;
+
+public class Exercise10 {
+    public static void main(String[] args) {
+
+        System.out.println(14/1.6 / (45+30/60)/60 + "mph");
+
+    }
+}
