42ffdd237b6763514789b066fc02fcc348aaa92a
JiangK-123 <2984259709@qq.com>
2024-11-13 00:09:48 +0800

    Update Exercise12.java

--- Chapter1/Exercise12.java
+++ Chapter1/Exercise12.java
@@ -1,1 +1,11 @@
+package com.HJK.hello;
 
+import java.util.Scanner;
+
+public class Exercise12 {
+    public static void main(String[] args) {
+
+        System.out.println(24*1.6/ (60+40+35/60)/60 + "km/h");
+
+    }
+}
