a7cd28a505f58d23cc4d27c8cab992e8093644a5
JiangK-123 <2984259709@qq.com>
2024-11-18 09:28:12 +0800

    Update Task03.java

--- Week11/Task03.java
+++ Week11/Task03.java
@@ -1,1 +1,16 @@
+package com.HJK.hello;
 
+import java.util.Scanner;
+
+public class Task03 {
+    public static void main(String[] args) {
+        int [][] matrix = new int[3][3];
+
+        for(int row=0;row<matrix.length;row++){
+            for(int column=0;column<matrix[0].length;column++){
+                matrix[row][column]=(int)(Math.random()*20);
+            }
+        }
+        System.out.println(matrix[0][0]);
+    }
+}
