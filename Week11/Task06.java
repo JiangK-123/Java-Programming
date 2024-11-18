8e850bcdffe7bd4c8af8c9bb20e61278320fee72
JiangK-123 <2984259709@qq.com>
2024-11-18 09:36:57 +0800

    Update Task06.java

--- Week11/Task06.java
+++ Week11/Task06.java
@@ -1,1 +1,34 @@
+package com.HJK.hello;
 
+import java.util.Scanner;
+
+public class Task06 {
+    public static void main(String[] args) {
+        int [][] matrix = new int[3][3];
+
+        for(int row=0;row<matrix.length;row++){
+            for(int column=0;column<matrix[0].length;column++){
+                matrix[row][column]=(int)(Math.random()*20);
+            }
+        }
+
+        int maxOfRow=0;
+        int indexofMaxRow=0;
+        for(int row=0;row<matrix.length;row++){
+            for(int column=0;column<matrix[0].length;column++){
+                maxOfRow+=matrix[0][column];
+            }
+        }
+        int totalOfThisRow=0;
+        for(int row=0;row<matrix.length;row++){
+            for(int column=0;column<matrix[0].length;column++){
+                totalOfThisRow+=matrix[row][column];
+            }
+            if(totalOfThisRow>maxOfRow){
+                maxOfRow=totalOfThisRow;
+                indexofMaxRow=row;
+            }
+        }
+        System.out.println("Row "+indexofMaxRow+" has the maximum sum of "+maxOfRow);
+    }
+}
